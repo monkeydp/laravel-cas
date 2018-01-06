21c7c75c91550b21a32f78d23135d4b2666bfe5e
iPotato.Simba <monkeydpotato@gmail.com>
2018-01-06 21:18:42 +0800

    handleLogoutRequests

--- src/routes.php
+++ src/routes.php
@@ -3,8 +3,14 @@
 use \Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Route;
 
-Route::any('/cas/logout', function () {
+Route::get('/cas/logout', function () {
+
     phpCAS::logout([
         'service' => Config::get('cas.logout_redirect_to')
     ]);
+});
+
+Route::post('/cas/logout', function () {
+
+    phpCAS::handleLogoutRequests(true, Config::get('cas.allowed_clients'));
 });
