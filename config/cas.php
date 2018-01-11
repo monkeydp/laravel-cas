34407b48dec16a88a198093c7f1995a6438c50d4
iPotato.Simba <monkeydpotato@gmail.com>
2018-01-11 09:54:32 +0800

    *

--- config/cas.php
+++ config/cas.php
@@ -11,10 +11,10 @@ $CAS_ALLOWED_CLIENTS = env('CAS_ALLOWED_CLIENTS');
 
 return [
     'server_version'     => (string)env('CAS_SERVER_VERSION', '3.0'),
-    'server_host'        => (string)env('CAS_SERVER_HOST', 'your.cas.server.host'),
+    'server_host'        => (string)env('CAS_SERVER_HOST', 'your.cas-server.host'),
     'server_port'        => (int)env('CAS_SERVER_PORT', 8443),
-    'server_uri'         => (string)env('CAS_SERVER_URI', '/path/to/cas/project'),
-    'server_cert'        => (string)env('CAS_SERVER_CERT', '/path/to/cert'),
+    'server_uri'         => (string)env('CAS_SERVER_URI', ''),
+    'server_cert'        => (string)env('CAS_SERVER_CERT', ''),
     'allowed_clients'    => (array)($CAS_ALLOWED_CLIENTS ? explode(',', $CAS_ALLOWED_CLIENTS) : []),
     'logout_redirect_to' => (string)env('CAS_LOGOUT_REDIRECT_TO', 'localhost'),
 ];
